2ee8aa6b556077c74f9ed315b61333e5569d788c
Shogo <vanitas710@yahoo.co.jp>
2021-01-09 02:56:50 +0900

    スクロールできるようにした

--- scrollView_keyboard/ViewController.swift
+++ scrollView_keyboard/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myScrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // スクロールビューの領域を指定する
+        let scrollFrame = CGRect(x:0, y:20,width: view.frame.width,height: view.frame.height-20)        // ステータスバーの高さより下
+        myScrollView.frame = scrollFrame
+        
+        // コンテンツのサイズを指定する
+        let contentRenct = contentView.bounds
+        myScrollView.contentSize = CGSize(width: contentRenct.width, height: contentRenct.height)
+        
+        
     }
 
 
